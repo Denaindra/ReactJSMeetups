1fd508ec429bafb6dac75e217b3b8e7c548df6e8
gayan <mgdperera@gmail.com>
2021-11-10 07:44:15 +0530

    add main navigation

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetup from "./pages/AllMeetup";
 import NewMeetup from "./pages/NewMeetup";
 import Favourite from "./pages/Favourite";
+import MainNavigation from "./componet/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetup />} />
         <Route path="/new-meetup" element={<NewMeetup />} />
-        <Route path="/favourite" element={<Favourite />} />
+        <Route path="/favorites" element={<Favourite />} />
       </Routes>
     </div>
   );
