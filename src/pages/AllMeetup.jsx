4223bc5756b26f73db06185de9ac16a80da447b4
gayan <mgdperera@gmail.com>
2021-11-10 07:29:13 +0530

    add react route

---
+++ src/pages/AllMeetup.jsx
@@ -0,0 +1,9 @@
+const AllMeetup = () => {
+    return (
+        <div>
+            <h1>All meetup</h1>
+        </div>
+    );
+}
+
+export default AllMeetup;
---
+++ src/pages/Favourite.jsx
@@ -0,0 +1,9 @@
+const Favourite = () => {
+    return (
+        <div>
+            <h1>favourite meetup</h1>
+        </div>
+    );
+}
+
+export default Favourite;
